9d6156e5f268925846907663d53a3db038586cfa
DESKTOP-DFJD6GC\Joel van der werf <joelnl74@gmail.com>
2019-08-18 21:04:22 +0200

    fbx test

--- HydroEngine/src/Graphics/FBX/FBX.cpp
+++ HydroEngine/src/Graphics/FBX/FBX.cpp
@@ -15,7 +15,59 @@ namespace Hydro
 		ios = FbxIOSettings::Create(lSdkManager, IOSROOT);
 		lSdkManager->SetIOSettings(ios);
 
+		FbxImporter* lImporter = FbxImporter::Create(lSdkManager, "");
+		
+		// Use the first argument as the filename for the importer.
+		if (!lImporter->Initialize("Resources/fbx/test.fbx", -1, lSdkManager->GetIOSettings())) {
+			printf("Call to FbxImporter::Initialize() failed.\n");
+			printf("Error returned: %s\n\n", lImporter->GetStatus().GetErrorString());
+			
+			return false;
+		}
+		lScene = FbxScene::Create(lSdkManager, "myScene");
+
+		// Import the contents of the file into the scene.
+		lImporter->Import(lScene);
+
+		// The file is imported, so get rid of the importer.
+		lImporter->Destroy();
+
+		FbxNode* lRootNode = lScene->GetRootNode();
+		if (lRootNode) {
+			for (int i = 0; i < lRootNode->GetChildCount(); i++)
+				PrintNode(lRootNode->GetChild(i));
+		}
+
 		return true;
+	}
+	void FBX::PrintNode(FbxNode* pNode)
+	{
+			PrintTabs();
+			const char* nodeName = pNode->GetName();
+			FbxDouble3 translation = pNode->LclTranslation.Get();
+			FbxDouble3 rotation = pNode->LclRotation.Get();
+			FbxDouble3 scaling = pNode->LclScaling.Get();
+
+			// Print the contents of the node.
+			printf("<node name='%s' translation='(%f, %f, %f)' rotation='(%f, %f, %f)' scaling='(%f, %f, %f)'>\n",
+				nodeName,
+				translation[0], translation[1], translation[2],
+				rotation[0], rotation[1], rotation[2],
+				scaling[0], scaling[1], scaling[2]
+			);
+			numTabs++;
+
+			// Print the node's attributes.
+			for (int i = 0; i < pNode->GetNodeAttributeCount(); i++)
+				PrintAttribute(pNode->GetNodeAttributeByIndex(i));
+
+			// Recursively print the children.
+			for (int j = 0; j < pNode->GetChildCount(); j++)
+				PrintNode(pNode->GetChild(j));
+
+			numTabs--;
+			PrintTabs();
+			printf("</node>\n");
 	}
 }
 
--- HydroEngine/src/Graphics/FBX/FBX.h
+++ HydroEngine/src/Graphics/FBX/FBX.h
@@ -7,9 +7,61 @@ namespace Hydro
 	public:
 		FBX();
 		bool Setup();
+		void PrintNode(FbxNode* pNode);
 
 	private:
 		FbxManager* lSdkManager;
 		FbxIOSettings *ios;
+		FbxImporter *lImporter;
+		FbxScene* lScene;
+
+		/* Tab character ("\t") counter */
+		int numTabs = 0;
+
+		/**
+		 * Print the required number of tabs.
+		 */
+		void PrintTabs() {
+			for (int i = 0; i < numTabs; i++)
+				printf("\t");
+		}
+
+		void PrintAttribute(FbxNodeAttribute* pAttribute) {
+			if (!pAttribute) return;
+
+			FbxString typeName = GetAttributeTypeName(pAttribute->GetAttributeType());
+			FbxString attrName = pAttribute->GetName();
+			PrintTabs();
+			// Note: to retrieve the character array of a FbxString, use its Buffer() method.
+			printf("<attribute type='%s' name='%s'/>\n", typeName.Buffer(), attrName.Buffer());
+		}
+		/**
+		 * Return a string-based representation based on the attribute type.
+		 */
+		FbxString GetAttributeTypeName(FbxNodeAttribute::EType type) {
+			switch (type) {
+			case FbxNodeAttribute::eUnknown: return "unidentified";
+			case FbxNodeAttribute::eNull: return "null";
+			case FbxNodeAttribute::eMarker: return "marker";
+			case FbxNodeAttribute::eSkeleton: return "skeleton";
+			case FbxNodeAttribute::eMesh: return "mesh";
+			case FbxNodeAttribute::eNurbs: return "nurbs";
+			case FbxNodeAttribute::ePatch: return "patch";
+			case FbxNodeAttribute::eCamera: return "camera";
+			case FbxNodeAttribute::eCameraStereo: return "stereo";
+			case FbxNodeAttribute::eCameraSwitcher: return "camera switcher";
+			case FbxNodeAttribute::eLight: return "light";
+			case FbxNodeAttribute::eOpticalReference: return "optical reference";
+			case FbxNodeAttribute::eOpticalMarker: return "marker";
+			case FbxNodeAttribute::eNurbsCurve: return "nurbs curve";
+			case FbxNodeAttribute::eTrimNurbsSurface: return "trim nurbs surface";
+			case FbxNodeAttribute::eBoundary: return "boundary";
+			case FbxNodeAttribute::eNurbsSurface: return "nurbs surface";
+			case FbxNodeAttribute::eShape: return "shape";
+			case FbxNodeAttribute::eLODGroup: return "lodgroup";
+			case FbxNodeAttribute::eSubDiv: return "subdiv";
+			default: return "unknown";
+			}
+		}
 	};
 }
